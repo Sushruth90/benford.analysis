fe74194860834f52a6f514abf35dc9ca36891508
Sushruth90 <42514247+Sushruth90@users.noreply.github.com>
2023-09-25 22:34:25 -0400

    Add files via upload

---
+++ benford.analysis.R
@@ -0,0 +1,24 @@
+# load libraries
+install.packages("gtrendsR")
+install.packages("reshape2")
+install.packages("dplyr")
+install.packages("benford.analysis")
+install.packages("pdftools")
+install.packages("stringr")
+
+library(stringr)
+library(gtrendsR)
+library(reshape2)
+library(dplyr)
+library(benford.analysis)
+library(pdftools)
+# read data
+txt <- pdf_text("C:/Users/sushr/OneDrive/Desktop/Common risk factors in the cross-section of corporate bond returns.pdf")
+txt
+# filter out numbers
+numeric_values <- as.numeric(unlist(str_extract_all(txt, "\\d+")))
+# perform benford analysis
+trends = benford(numeric_values, number.of.digits = 1, discrete = T, sign = "positive") 
+trends
+# plot results
+plot(trends)
